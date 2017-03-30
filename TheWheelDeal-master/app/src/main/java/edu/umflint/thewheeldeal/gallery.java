38b61a7a67cc23c55634ce6ebae13fe434133dd9
cbindsch <cbindsch@umflint.edu>
2017-03-29 22:58:49 -0400

    code

---
+++ TheWheelDeal-master/app/src/main/java/edu/umflint/thewheeldeal/gallery.java
@@ -0,0 +1,8 @@
+package edu.umflint.thewheeldeal;
+
+/**
+ * Created by crist_000 on 3/29/2017.
+ */
+
+public class gallery {
+}
---
+++ TheWheelDeal-master/app/src/main/java/edu/umflint/thewheeldeal/login.java
@@ -0,0 +1,10 @@
+package edu.umflint.thewheeldeal;
+
+import android.app.Activity;
+
+/**
+ * Created by crist_000 on 3/29/2017.
+ */
+
+public class login extends Activity {
+}
